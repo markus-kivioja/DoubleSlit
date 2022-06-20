d6dfd77f18a5d13ea3735dbd31196d97d3f172a0
Markus Kivioja <markus.kivioja@basemark.com>
2022-06-20 10:49:23 +0300

    Improved the potential wall drawing and erasing.

--- schrode.py
+++ schrode.py
@@ -74,34 +74,31 @@ even_psi_h = np.zeros((N, N), dtype=np.complex64)
 odd_psi_h = np.zeros((N, N), dtype=np.complex64)
 V_h = np.zeros((N, N), dtype=np.float32)
 
-def set_initial_condition():
-    # Initialize the wave function and potential
-    global even_psi_h, odd_psi_h
-    global even_psi_d, odd_psi_d
-    sigma = 1 / 12.
-    pos0_x = 3 * sigma
-    pos0_y = 0.5
-    k = -18.0 * 2*np.pi
-    for x_id in range(N):
-        for y_id in range(N):
-            x = x_id * h
-            y = y_id * h
-            diff_x = x - pos0_x
-            diff_y = y - pos0_y
-            psi = np.exp(-(diff_x*diff_x + diff_y*diff_y) / (2 * sigma * sigma))
-            phase = np.sin(k*x) + 1j*np.cos(k*x)
-            even_psi_h[y_id, x_id] = psi * phase
-            odd_psi_h[y_id, x_id] = psi * phase
+# Initialize the wave function and potential
+sigma = 1 / 12.
+pos0_x = 3 * sigma
+pos0_y = 0.5
+k = -18.0 * 2*np.pi
+for x_id in range(N):
+    for y_id in range(N):
+        x = x_id * h
+        y = y_id * h
+        diff_x = x - pos0_x
+        diff_y = y - pos0_y
+        psi = np.exp(-(diff_x*diff_x + diff_y*diff_y) / (2 * sigma * sigma))
+        phase = np.sin(k*x) + 1j*np.cos(k*x)
+        even_psi_h[y_id, x_id] = psi * phase
+        odd_psi_h[y_id, x_id] = psi * phase
 
-    # Normalize the wave function
-    normSq = integrate_normSq(even_psi_h)
-    even_psi_h /= np.sqrt(normSq)
-    odd_psi_h /= np.sqrt(normSq)
-    normSq = integrate_normSq(odd_psi_h)
+# Normalize the wave function
+normSq = integrate_normSq(even_psi_h)
+even_psi_h /= np.sqrt(normSq)
+odd_psi_h /= np.sqrt(normSq)
+normSq = integrate_normSq(odd_psi_h)
 
-    # Copy data from CPU to GPU
-    even_psi_d = cp.array(even_psi_h.reshape(-1), dtype=cp.complex64)
-    odd_psi_d = cp.array(odd_psi_h.reshape(-1), dtype=cp.complex64)
+# Copy data from CPU to GPU
+even_psi_d = cp.array(even_psi_h.reshape(-1), dtype=cp.complex64)
+odd_psi_d = cp.array(odd_psi_h.reshape(-1), dtype=cp.complex64)
 
 def add_double_slit():
     global V_h
@@ -111,7 +108,6 @@ def add_double_slit():
     V_h[int(N * 0.59):N, int(N * 0.8):int(N * 0.82)] = 999999.0
     V_d = cp.array(V_h.reshape(-1), dtype=cp.float32)
 
-set_initial_condition()
 add_double_slit()
 
 iters_per_frame = 2
@@ -125,16 +121,15 @@ def time_integrate(i, *args):
             # Update an even time step
             take_timestep(grid_size, block_size, (even_psi_d, odd_psi_d, V_d, cp.float32(inv_hSq), cp.float32(dt), cp.int32(N)))
 
-        # Compute the probability/phase and visualize
-        comp_normSq_phase(grid_size, block_size, (normSq_d, phase_d, even_psi_d, cp.int32(N)))
-    
+    # Compute the probability/phase and visualize
+    comp_normSq_phase(grid_size, block_size, (normSq_d, phase_d, even_psi_d, cp.int32(N)))
     im.set_data(V_d.get().reshape((N, N)) + normSq_d.get().reshape((N, N)))
     return im,
 
 
 
 
-############ Add the visualization and user interface ############
+############ Add the user interface and visualization ############
 normSq_d = cp.zeros(N*N, dtype=cp.float32)
 phase_d = cp.zeros(N*N, dtype=cp.float32)
 fig, ax = plt.subplots()
@@ -152,8 +147,9 @@ def play_toggle_clicked(event):
         play_toggle_button.label.set_text("Play")
     play_toggle_button.canvas.draw_idle()
 def reset_state_clicked(event):
-    global even_psi_d, odd_psi_d, V_d
-    set_initial_condition()
+    global even_psi_d, odd_psi_d
+    even_psi_d = cp.array(even_psi_h.reshape(-1), dtype=cp.complex64)
+    odd_psi_d = cp.array(odd_psi_h.reshape(-1), dtype=cp.complex64)
 def clear_v_clicked(event):
     global V_h
     global V_d
@@ -179,15 +175,19 @@ def mouse_moves(event):
     global left_is_down
     global right_is_down
     global V_d
+    if event.xdata is None or event.ydata is None:
+        return
+    x = int(event.xdata)
+    y = int(event.ydata)
     if left_is_down:
-        V_h[int(event.ydata), int(event.xdata)] = 999999.0
+        for x_offset in range(-2,2):
+            for y_offset in range(-2,2):
+                V_h[y + y_offset, x + x_offset] = 999999.0
         V_d = cp.array(V_h.reshape(-1), dtype=cp.float32)
     elif right_is_down:
-        V_h[int(event.ydata), int(event.xdata)] = 0.0
-        V_h[int(event.ydata)+1, int(event.xdata)] = 0.0
-        V_h[int(event.ydata)-1, int(event.xdata)] = 0.0
-        V_h[int(event.ydata), int(event.xdata)+1] = 0.0
-        V_h[int(event.ydata), int(event.xdata)-1] = 0.0
+        for x_offset in range(-3,3):
+            for y_offset in range(-3,3):
+                V_h[y + y_offset, x + x_offset] = 0.0
         V_d = cp.array(V_h.reshape(-1), dtype=cp.float32)
 
 button_width = 0.17
@@ -212,5 +212,5 @@ fig.canvas.mpl_connect('button_press_event', mouse_down)
 fig.canvas.mpl_connect('button_release_event', mouse_up)
 fig.canvas.mpl_connect('motion_notify_event', mouse_moves)
 
-ani = animation.FuncAnimation(fig, time_integrate, interval=1, blit=True)
+ani = animation.FuncAnimation(fig, time_integrate, interval=0, blit=True)
 plt.show()
